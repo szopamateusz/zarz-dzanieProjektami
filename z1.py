6076a083a07209e25eb37beb5f10fe50bb7878de
KamilPK <you@example.com>
2018-04-16 11:40:04 +0200

    blabla2

--- z1.py
+++ z1.py
@@ -2,11 +2,11 @@ import turtle
 
 def Prostokat(color):
     t.pencolor(color)
-    t.forward(100);
+    t.forward(120);
     t.right(90);
     t.forward(50);
     t.right(90);
-    t.forward(100);
+    t.forward(120);
     t.right(90);
     t.forward(50);
 
